87f87ebe42a4dcf4ec0028cbb502200c12ae9392
Andrew B <andrew.baum435@gmail.com>
2023-02-23 19:12:08 -0500

    Increment version number for serial fixes

--- gameKeyCompanion.py
+++ gameKeyCompanion.py
@@ -20,7 +20,7 @@
 from gk_ui_main import MainUI
 from PyQt5 import QtWidgets as QtW
 import platform
-version = "2.0.0"
+version = "2.0.1"
 
 if platform.system() == 'Linux':
     print("Linux detected!")
